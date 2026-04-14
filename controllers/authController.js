bdd0d76ac257b74d820912131d41e0404abb2130
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-04-14 22:23:36 +0530

    feat(auth): add Google login, handle new/existing users, improve JWT cookie flow and security checks

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,57 @@
 const User = require("../models/User");
+const {OAuth2Client} = require("google-auth-library");
 const generateToken = require("../utils/generateToken");
 const cookieOptions = require("../utils/cookieOptions");
+
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+
+exports.googleLogin = async (req,res)=>{
+  try{
+    const {token}=req.body;
+
+    // 1. Verify token
+    const ticket=await client.verifyIdToken({
+      idToken: token,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    })
+
+    const payload=ticket.getPayload();
+
+    // 2. Check if user exists
+    let user = await User.findOne({
+      email: payload.email,
+    })
+
+    // 3. Create user if not exists
+    if(!user){
+      user = await User.create({
+        name: payload.name,
+        email: payload.email,
+        googleId: payload.sub,
+        password: crypto.randomBytes(20).toString("hex"), 
+      })
+    }
+
+    // 4. Generate token
+    const jwtToken=generateToken(user._id);
+
+    // 5. Send response (cookie only)
+    res
+      .cookie("token", jwtToken, cookieOptions)
+      .status(200)
+      .json({
+        success: true,
+        message: "User logged in successfully",
+      });
+
+  } catch(err){
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+      error: err.message,
+    })
+  }
+}
 
 // ================= SIGNUP =================
 exports.signup = async (req, res) => {
