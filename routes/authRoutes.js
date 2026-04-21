39a7c5fb84c57cd13e89067203fd98c0ac8f8e63
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-04-21 22:57:49 +0530

    feat(auth): Update authRoute by adding rate-limiter

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,14 +7,19 @@ const {
   logout,
   googleLogin,
   refreshToken,
+  verifyEmail,
 } = require("../controllers/authController");
 
-router.post("/signup", signup);
-router.post("/login", login);
+const { loginLimiter, signupLimiter } = require("../middlewares/rateLimiter");
+
+// ✅ Apply rate limiting
+router.post("/signup", signupLimiter, signup);
+router.post("/login", loginLimiter, login);
+
 router.post("/logout", logout);
 router.post("/google-login", googleLogin);
-
-// ✅ NEW ROUTE
 router.post("/refresh-token", refreshToken);
+
+router.get("/verify-email", verifyEmail);
 
 module.exports = router;
